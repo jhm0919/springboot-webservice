7ae14c92be4cec498c6bf1227230424d6eb83e19
janghaemin <jhm0919@naver.com>
2024-02-20 17:15:42 +0900

    20240220

--- src/main/java/com/jhm/springbootwebservice/config/auth/LoginUser.java
+++ src/main/java/com/jhm/springbootwebservice/config/auth/LoginUser.java
@@ -6,6 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * @Target
  * 이 어노테이션이 생성될 수 있는 위치를 지정
  * PARAMETER로 지정했으니 메소드의 파라미터로 선언된 객체에서만 사용할 수 있음
  * 이 외에도 클래스 선언문에 쓸 수 있는 TYPE 등이 있음
--- src/main/java/com/jhm/springbootwebservice/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jhm/springbootwebservice/config/auth/dto/OAuthAttributes.java
@@ -63,7 +63,7 @@ public class OAuthAttributes {
     /**
      * User 엔티티를 생성
      * OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할때임
-     * 가입할 대의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용
+     * 가입할 때의 기본 권한을 GUEST로 주기 위해서 role 빌더값에는 Role.GUEST를 사용
      * OAuthAttributes 클래스 생성이 끝났으면 같은 패키지에 SessionUser 클래스를 생성
      * @return
      */
@@ -72,7 +72,8 @@ public class OAuthAttributes {
                 .name(name)
                 .email(email)
                 .picture(picture)
-                .role(Role.GUEST)
+//                .role(Role.GUEST)
+                .role(Role.USER)
                 .build();
     }
 }
