93d5b8b48d9ae0c539009d7702b0927fde27a9ec
janghaemin <haemin0919@gmail.com>
2024-06-17 21:30:23 +0900

    20240617

--- src/main/java/com/jhm/springbootwebservice/domain/posts/PostType.java
+++ src/main/java/com/jhm/springbootwebservice/domain/posts/PostType.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum PostType {
-    NOTICE("notice", "공지사항"),
-    FREE("free", "자유게시판"),
-    BOARD1("board1", "게시판1"),
-    BOARD2("board2", "게시판2"),
-    BOARD3("board3", "게시판3");
+    NOTICE("NOTICE", "공지사항"),
+    FREE("FREE", "자유게시판"),
+    BOARD1("BOARD1", "게시판1"),
+    BOARD2("BOARD2", "게시판2"),
+    BOARD3("BOARD3", "게시판3");
 
     private final String key;
     private final String title;
